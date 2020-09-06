8b62549a4d17868550cc5f8d7dc2389ff941fe91
JoneCoder <mohummadshorifulislam@gmail.com>
2020-09-06 12:22:53 +0600

    login conrtroller update

--- admin/app/Http/Controllers/Auth/LoginController.php
+++ admin/app/Http/Controllers/Auth/LoginController.php
@@ -8,33 +8,26 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+
+    protected $maxAttempts  = 5;
+    protected $decayMinutes = 1;
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+
+    public function clearThrottle($request)
+    {
+        $this->clearLoginAttempts($request);
     }
 }
